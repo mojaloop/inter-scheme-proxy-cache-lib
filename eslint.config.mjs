81cd1cd1f990f76a15500fe7c9d391f4ac15b3ba
geka.evk <geka.evk@gmail.com>
2025-03-20 12:16:02 +0000

    feat(csi-1266): fixed eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,45 +4,42 @@ import tsParser from '@typescript-eslint/parser';
 import globals from 'globals';
 
 export default [
-    {
-        files: ['**/*.ts'], // Target TypeScript files
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: 2022, // Support modern JavaScript
-            sourceType: 'module',  // Enable ESModules
-            globals: {
-                ...globals.node,
-            }
-        },
-        plugins: {
-            '@typescript-eslint': ts,
+  {
+    files: ['**/*.ts'], // Target TypeScript files
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022, // Support modern JavaScript
+      sourceType: 'module', // Enable ESModules
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      ...js.configs.recommended.rules, // Include recommended JavaScript rules
+      ...ts.configs.recommended.rules, // Include recommended TypeScript rules
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-console': 'error',
+      indent: ['error', 2, { SwitchCase: 1 }],
+    },
+    ignores: ['**/coverage', '**/templates'],
+    overrides: [
+      {
+        files: ['test/**/*.ts'],
+        env: {
+          jest: true,
         },
         rules: {
-            ...js.configs.recommended.rules, // Include recommended JavaScript rules
-            ...ts.configs.recommended.rules, // Include recommended TypeScript rules
-            '@typescript-eslint/no-explicit-any': 'error',
-            'no-console': 'error',
-            indent: ['error', 2, { SwitchCase: 1 }],
+          // add here any rules specific to test files
         },
-        env: {
-            node: true,
-            jest: true,
-        },
-        ignores: ["**/coverage", "**/templates"],
-        overrides: [
-            {
-                files: ['test/**/*.ts'],
-                env: {
-                    jest: true,
-                },
-                rules: {
-                    // add here any rules specific to test files
-                },
-            },
-            {
-                extends: ['plugin:@typescript-eslint/disable-type-checked'],
-                files: ['./**/*.js'],
-            },
-        ],
-    },
-]
+      },
+      {
+        extends: ['plugin:@typescript-eslint/disable-type-checked'],
+        files: ['./**/*.js'],
+      },
+    ],
+  },
+];
